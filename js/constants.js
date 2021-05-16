25d54f00e0755b78ae29e10b9cdd46921f617006
Rob Hatcher <rhatcher94@outlook.com>
2021-05-15 20:09:28 -0400

    Complete V2
    Add favicon, separate sections into js constants.

--- js/constants.js
+++ js/constants.js
@@ -1,29 +1,62 @@
 site_title = "Rob Hatcher"
 main_heading_text = "Web Developer"
-sub_heading_text = "I'm a full stack web developer with a drive to always be improving."
+sub_heading_text =
+	"I'm a full stack web developer with a drive to always be improving."
 
 nav_menu_items = [
-    {
-        "title": "ABOUT",
-        "link": "#about"
-    },   
-    {
-        "title": "PORTFOLIO",
-        "link": "#portfolio"
-    },  
-    {
-        "title": "CONTACT",
-        "link": "#contact"
-    },  
+	{
+		title: "ABOUT",
+		link: "#about",
+	},
+	{
+		title: "PORTFOLIO",
+		link: "#portfolio",
+	},
+	{
+		title: "CONTACT",
+		link: "#contact",
+	},
 ]
 
-about_text_1 = "Hi, I'm Rob. Nice to meet you. \
+about_text_1 =
+	"Hi, I'm Rob. Nice to meet you. \
 I am a full stack developer and graduate of the Computer Programmer Analyst Program at St. Lawrence college. \
 Currently the projects I work on are using Vue.js and typescript for a front end and a Laravel PHP with MySQL for the back end."
-                
-about_text_2 = "If i'm not coding at work you can usually find me coding at home (which is sometimes the same place). \
+
+about_text_2 =
+	"If i'm not coding at work you can usually find me coding at home (which is sometimes the same place). \
 I enjoy researching and discovering new technologies and techniques to improve my work flow and allow me to produce better code."
+
+/**
+ * Protfolio Projects list
+ * title: the title of the project.
+ * desc: the description of the project.
+ * code_link: a link to the project source code.
+ * app_link: a link to the project where it is hosted, or null if none.
+ */
+portfolio_projects = [
+	{
+		title: "Soup Kitchen",
+		desc: "An inventory system for a fictional Soup Kitchen, developed in collaboration with fellow students at St.Lawrence College using agile scrums.",
+		code_link: "https://github.com/Rhatcher94/SoupKitchen",
+		app_link: null,
+	},
+	{
+		title: "HearthCards",
+		desc: "A simple web app allowing you to view hearthstone cards with filters and create a deck.",
+		code_link: "https://github.com/Rhatcher94/HearthCards",
+		app_link: "https://rhatcher94.github.io/HearthCards/#",
+	},
+	{
+		title: "MERN eCommerce",
+		desc: `A web app created as a personal project intended to be an updated version of a local pet shops website. 
+                The back end was created using Express and mongoDB. The front end was created using bootstrap, react, redux and react-router.`,
+		code_link: "https://github.com/Rhatcher94/MERN_eCommerce",
+		app_link: null,
+	},
+]
 
 phone_number_text = "(343)363-9974"
 email_address_text = "rhatcher94@outlook.com"
-                
+
+copyright_text = "Created by Rob Hatcher @ robhatcher.com"
